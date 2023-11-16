e4a4cc822b39a2a28d5094189af4358239eee4eb
Sebastian Pająk <42960067+Sebastian2103@users.noreply.github.com>
2023-11-16 12:20:37 +0100

    poprawione

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="Content-Language" content="pl" />
+        <meta name="Author" content="Sebastian Pająk" />
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div id="navbar">
+            <a href="html/history.html">Historia serii</a>
+            <a href="">Jednostki</a>
+            <a href="">Bohaterowie</a>
+            <a href="">Zamki</a>
+        </div>
+        <?php 
+        include("glowna.html")
+        ?>
+    </body>
+</html>

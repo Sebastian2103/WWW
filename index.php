165f34373a2e625489451b2361716f033ffb37b0
Sebastian Pająk <42960067+Sebastian2103@users.noreply.github.com>
2023-12-07 16:22:40 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,34 +7,61 @@
         <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
         <meta http-equiv="Content-Language" content="pl" />
         <meta name="Author" content="Sebastian Pająk" />
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type ="text/css" href="style.css">
+        <script src="js/button.js" defer></script>
     </head>
     <body>
-		<?php
-			include('cfg.php');
-			include('showpage.php')
-		?>
+
+        <!-- menu boczne -->
         <div id="navbar">
             <a href="index.php?idp=">Strona Główna</a>
             <a href="index.php?idp=history">Historia serii</a>
-            <a href="">Jednostki</a>
-            <a href="">Bohaterowie</a>
+            <button id="dropdown-btn">Jednostki
+             <!-- <i class="fa fa-caret-down"></i> -->
+             </button>
+            <div class="dropdown-container">
+                <a href="index.php?idp=akademia">Akademia</a>
+                <a href="index.php?idp=necropolis">Nekropolis</a>
+                <a href="index.php?idp=sylwan">Sylwan</a>
+                <a href="index.php?idp=loch">Loch</a>
+                <a href="index.php?idp=inferno">Inferno</a>
+                <a href="index.php?idp=przystan">Przystań</a>
+            </div>
+            <a href="index.php?idp=bohater">Bohaterowie</a>
             <a href="">Zamki</a>
         </div>
-        <?php 
-		if($_GET['idp']=='')
-		{PokazPodstrone(1);}
-		
-       // $strona = '';
-        //if($_GET['idp']=='')
-       // {$strona = './html/glowna.html';}
-       // if($_GET['idp']=='history')
-       // {$strona = './html/history.html';}
-//
-      //  if(file_exists($strona))
-      //  {
-      //      include($strona);
-      //  }
+        <?php
+                //załadowanie skryptu połączenia z baza danych
+                include('cfg.php');
+                    //załadowanie metody PokazPodstrone
+                    include('showpage.php');
+                //zdefioniowanie zmiennej strona ktora będzie słuzyć do 
+                $strona = '';
+                //warunki które za pomoca odpowiedniej wartości idp beda wyświetlały inne podstrony
+                if($_GET['idp']=='')
+                {echo PokazPodstrone(1);}
+                if($_GET['idp']=='history')
+                {echo PokazPodstrone(2);}       
+                if($_GET['idp']=='akademia')
+                {echo PokazPodstrone(3);}
+                if($_GET['idp']=='necropolis')
+                {echo PokazPodstrone(4);}     
+                if($_GET['idp']=='loch')
+                {echo PokazPodstrone(5);}
+                if($_GET['idp']=='sylwan')
+                {echo PokazPodstrone(6);}
+                if($_GET['idp']=='inferno')
+                {echo PokazPodstrone(7);}
+                if($_GET['idp']=='przystan')
+                {echo PokazPodstrone(8);}
+                if($_GET['idp']=='bohater')
+                {echo PokazPodstrone(9);}
+
+            // warunek sprawdzaący czy dany klucz istnieje, jesli istnieje kor
+            if(file_exists($strona))
+            {
+                include($strona);
+            }
         ?>
     </body>
 </html>
--- showpage.php
+++ showpage.php
@@ -1,24 +1,35 @@
 <?php
-	function PokazPodstrone($id)
-	{
-		$id_clear = htmlspecialchars($id);
-		
-		$querry = "SELECT * FROM page_list WHERE id='$id_clear' LIMIT 1";
-		$result = mysqli_query($query);
-		$row = mysql_fetch_array($result);
-		
-		//wywołanie strony bazy
-		
-		if(empty($row['id']))
-		{
-			$web='[nie_znaleziono_strony]';
-		}
-		else
-		{
-			$web = $row['page_content'];
-		}
-		return $web;
-	}
+#--------------------------------#
+# 1. Metoda pokazująca podstrony #
+#--------------------------------# 
+#
+# po sprawdzeniu czy podstrona istnieje wyświetla page_content z tabeli page_list
+#
+#
 
+function PokazPodstrone($id)
+{
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = '';
+	$baza = 'moja_strona';
+
+	$link = mysqli_connect($dbhost, $dbuser, $dbpass, $baza);
+	
+	$id_clear = htmlspecialchars($id);
+	$query = "SELECT * FROM page_list WHERE id='$id_clear' LIMIT 1";
+	$result = mysqli_query($link ,$query);
+	$row = mysqli_fetch_array($result);
+	
+	if(empty($row['id']))
+	{
+		$web = '[nie_znaleziono_strony]';
+	}
+	else
+	{		
+		$web = $row['page_content'];
+	}
+	return $web;
+}
 
 ?>
